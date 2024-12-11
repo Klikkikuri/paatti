78586ac39281342ef774b2febc08bfdf0ea28273
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2024-12-11 18:32:21 +0200

    feat(ui): Change site enabling-settings behind cog icon

--- popup/index.js
+++ popup/index.js
@@ -5,10 +5,23 @@ document.addEventListener("click", (e) => {
     browser.tabs
         .query({ active: true, currentWindow: true })
         .then((tabs) => {
-            if (e.target.id === "enabled") {
-                browser.tabs.sendMessage(tabs[0].id, {
-                    command: e.target.checked ? "replaceClickbaits" : "restoreClickbaits",
-                });
+            switch (e.target.id) {
+                case "enabled":
+                    browser.tabs.sendMessage(tabs[0].id, {
+                        command: e.target.checked ? "replaceClickbaits" : "restoreClickbaits",
+                    });
+                    break;
+                case "open-settings":
+                    const settingsElem = document.querySelector("#settings");
+                    if (settingsElem
+                        .classList
+                        .contains("hidden")
+                    ) {
+                        settingsElem.classList.remove("hidden");
+                    } else {
+                        settingsElem.classList.add("hidden");
+                    }
+                    break;
             }
         });
 });
