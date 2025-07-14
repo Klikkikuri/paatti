60e7d90f5b0cc1a981ddd7af3eb911644c5a2807
Juho <50268629+trkks@users.noreply.github.com>
2025-07-14 17:44:57 +0300

    fix(test data): Update suola and use correct CLI stage to hash URLs

--- test_data.sh
+++ test_data.sh
@@ -5,13 +5,13 @@ set -e
 # Remove old computed hashes.
 rm -f test_data/hashes.txt
 
-docker build -t runsuola --target runnative suola/
+docker build -t suolacli --target cli suola/
 
 # Run suola on the URLs found in test data file.
 for url in $(cat test_data/urls.txt);
 do
     echo !! Hashing: $url
-    docker run runsuola -sign -url $url | sed -n -e 's/^.*Signature: //p' >> test_data/hashes.txt
+    docker run suolacli -sign -url $url | sed -n -e 's/^.*Signature: //p' >> test_data/hashes.txt
 done
 
 python3 test_data/generate_data.py
