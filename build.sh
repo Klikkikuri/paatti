f0a48a9a7ab3e6ab4fdcb93df1f7166df7100617
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2025-12-22 20:18:16 +0200

    chore: Update suola

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git submodule init
 git submodule update
 
 # Build suola artifacts.
-docker build --target builder -t buildsuola suola/
+docker build --target wasm-builder -t buildsuola suola/
 docker run --mount type=bind,src=$(pwd)/suola/build/,dst=/app/build buildsuola
 
 ./generate_test_data.py
