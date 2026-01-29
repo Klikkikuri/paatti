c1ecbfa7dadc0fbe307f97d145fa36a7f529129f
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2026-01-29 22:27:41 +0200

    fix(On/Off): Fix the global on/off button functionality

--- src/contentScript.js
+++ src/contentScript.js
@@ -16,187 +16,26 @@
 // Use this to access this source file in the browser debugger.
 //debugger;
 
-// WORKAROUND: These variables will be used in "importing" from other JS files in main().
-let log,
-    extractArticleUrl,
-    noElementMatchesForQuerySelector,
-    noTitleMatchesForHash,
-    highlightElemConverted,
-    highlightElemOriginal;
-
-const ERROR_VARIANTS = {
-    UnknownError: "Unknown error",
-    noElementMatchesForQuerySelector: "Empty query result for title elements under selected parent",
-    noTitleMatchesForHash: "Found no conversion for given title in database",
-};
-
 const hrefSign = async (url) => {
     const urlObj = new URL(url, window.location.href);
     return await hashUrl(urlObj.href);
 }
 
-const canonicallyHashizeElem = async (titleData, querySelectors, link) => {
-    const titleElem = querySelectors
-        .map((x) => {
-            if (x === "") {
-                // Empty selector means the a-tag is expected to contain the
-                // title text itself.
-                return link;
-            } else {
-                return link.querySelector(x);
-            }
-        })
-        .find((x) => x != null);
-
-    if (!titleElem) {
-        throw {
-            variant: ERROR_VARIANTS.noElementMatchesForQuerySelector,
-            elem: link,
-        };
-    }
-
-    const articleUrl = await extractArticleUrl(link);
-    const linkHash = await hashUrl(articleUrl);
-
-
-    let match = null;
-    for (const entry of titleData.entries) {
-        if (entry.urls.filter((x) => x.sign === linkHash).length > 0) {
-            match = entry;
-            break;
-        }
-    }
-
-    if (!match) {
-        throw {
-            variant: ERROR_VARIANTS.noTitleMatchesForHash,
-            elem: link,
-            href: link.href,
-            hash: linkHash,
-        };
-    } else {
-        return {
-            titleElem: titleElem,
-            entry: match,
-        };
-    }
-};
-
-/**
- * Filter out the links not processed in backend.
- */
-const getReplaceableTitleElements = async (links, titleData, linkTitleQuerySelectors) => {
-    const elemPromises = links.map((x) => canonicallyHashizeElem(
-        titleData,
-        linkTitleQuerySelectors ?? [],
-        x));
-
-    const elems = [];
-    const errors = [];
-    for (const x of await Promise.allSettled(elemPromises)) {
-        if (x.status === "fulfilled") {
-            elems.push(x.value);
-        } else {
-            let err = x.reason;
-            switch (err.variant) {
-                case ERROR_VARIANTS.noElementMatchesForQuerySelector:
-                    await noElementMatchesForQuerySelector(err.elem);
-                    break;
-                case ERROR_VARIANTS.noTitleMatchesForHash:
-                    await noTitleMatchesForHash(err.elem);
-                    break;
-                default:
-                    err = {
-                        variant: ERROR_VARIANTS.UnknownError,
-                        data: err,
-                    };
-                    break
-            }
-            errors.push(err);
-        }
-    }
-    const errorStats = {};
-    for (const variant of Object.values(ERROR_VARIANTS)) {
-        errorStats[variant] = errors.filter((x) => x.variant === variant);
-    }
-    log(`There were ${errors.length} links not processed:`, errorStats);
-
-    return elems;
-};
-
-const harvestTitleElements = async (links, titleData, linkTitleQuerySelectors) => {
-    const xs = [];
-    for (const { titleElem, entry } of await getReplaceableTitleElements(links, titleData, linkTitleQuerySelectors)) {
-        if (!titleElem.getAttribute("__klikkikuri_original_title")) {
-            // Store the original title in memory for when wanting to convert
-            // back later if not already.
-            titleElem.setAttribute("__klikkikuri_original_title", titleElem.textContent);
-        }
-        xs.push({
-            titleElem,
-            originalTitle: titleElem.getAttribute("__klikkikuri_original_title"),
-            convertedTitle: entry.title,
-        });
-    }
-    return xs;
-};
-
-const processClickbaits = async (links, titleData, linkTitleQuerySelectors, f) => {
-    const SUPPORTED_SCHEMA_VERSION = "0.1.0";
-    if (titleData.schema_version != SUPPORTED_SCHEMA_VERSION) {
-        // TODO: What now? Link user to update page?
-        throw `The title data format is not compatible: version is ${titleData.schema_version} when expected ${SUPPORTED_SCHEMA_VERSION}. Update Paatti in order to fix.`;
-    }
-
-    const thisPageTitleData = await harvestTitleElements(links, titleData, linkTitleQuerySelectors);
-    for (const { titleElem, originalTitle, convertedTitle } of thisPageTitleData) {
-        await f(titleElem, { originalTitle, convertedTitle });
-    }
-
-    return thisPageTitleData;
-};
-
-const replaceClickbaits = async (links, titleData, linkTitleQuerySelectors) => {
-    let count = 0;
-    await processClickbaits(links, titleData, linkTitleQuerySelectors,
-        async (titleElem, { convertedTitle }) => {
-            titleElem.textContent = convertedTitle;
-            await highlightElemConverted(titleElem);
-            count += 1;
-        }
-    );
-    return count;
-};
-
-const restoreClickbaits = async (links, titleData, linkTitleQuerySelectors) => {
-    await processClickbaits(links, titleData, linkTitleQuerySelectors,
-        async (titleElem, { originalTitle }) => {
-            titleElem.textContent = originalTitle;
-            await highlightElemOriginal(titleElem);
-        }
-    );
-};
-
+let convertTitles;
 
 // Main.
 (async () => {
     // Import modules.
     const browser = (chrome || browser);
-    const { model, modelEvents } = await import(browser.runtime.getURL("src/model.js"));
+    const { model: model, modelEvents: modelEvents } = await import(browser.runtime.getURL("src/model.js"));
     const { controller } = await import(browser.runtime.getURL("src/controller.js"));
     const { getLogger } = await import(browser.runtime.getURL("src/utils.js"));
 
     const { rahtiStorage } =  await import(browser.runtime.getURL("src/rahti.js"));
 
+    const { highlightElemConverted, highlightElemOriginal } = await import(browser.runtime.getURL("src/conversionUtils.js"));
 
-    const cu = await import(browser.runtime.getURL("src/conversionUtils.js"));
-    extractArticleUrl = cu.extractArticleUrl;
-    noElementMatchesForQuerySelector = cu.noElementMatchesForQuerySelector;
-    noTitleMatchesForHash = cu.noTitleMatchesForHash;
-    highlightElemConverted = cu.highlightElemConverted;
-    highlightElemOriginal = cu.highlightElemOriginal;
-
-    log = getLogger("content_script");
+    const log = getLogger("content_script");
 
     // Remove the event from content script, as Chrome cries when it tries to
     // access browser.tabs sending conversion message.
@@ -210,7 +49,105 @@ const restoreClickbaits = async (links, titleData, linkTitleQuerySelectors) => {
         return;
     }
 
+    const rahti = await rahtiStorage;
+
+    if (!rahti) {
+        log("No Rahti data found, aborting conversion.", rahti);
+        return;
+    }
+
     const newsSite = window.location.hostname;
+
+    convertTitles = async (doRestore) => {
+        const siteRules = await model.read.getSiteRules(newsSite);
+        if (!siteRules) {
+            log(`No site rules found for '${newsSite}', aborting conversion.`);
+        }
+
+        for (const rule of siteRules) {
+            const containers = document.querySelectorAll(rule.container);
+
+            containers.forEach( (container) => {
+                const links = container.querySelectorAll(rule.link);
+                
+                links.forEach(async link => {
+                    // TODO: Might not work on javascript generated links (onclick etc.)
+                    const href = link.getAttribute('href');
+                    if (!href) {
+                        log("Skipping link without href:", link);
+                    }
+                    const urlHash = await hrefSign(href);
+                    // Store for debugging
+                    link.dataset.klikkikuriUrlHash = urlHash;
+
+                    const rahtiEntry = await rahti.get(urlHash)
+                    if (rahtiEntry) {
+                        const titleElem = rule.title ? container.querySelector(rule.title) : link;
+                        if (titleElem) {
+                            if (doRestore) {
+                                const convertedTitle = titleElem.dataset.klikkikuriConvertedTitle;
+                                const originalTitle = titleElem.dataset.klikkikuriOriginalTitle;
+
+                                titleElem.textContent = originalTitle
+                                await highlightElemOriginal(titleElem);
+                                
+                                // This needs to be removed so that the next
+                                // conversion will not be skipped.
+                                delete titleElem.dataset.klikkikuriConvertedTitle;
+
+                                log(`Restored title from '${convertedTitle}' to '${originalTitle}' for link:`, link);
+                            } else {
+                                const originalTitle = titleElem.textContent;
+                                const convertedTitle = rahtiEntry.title;
+
+                                if (titleElem.dataset.klikkikuriConvertedTitle === convertedTitle) {
+                                    // No action needed, already converted before.
+                                    return;
+                                }
+
+                                titleElem.dataset.klikkikuriOriginalTitle = originalTitle;
+                                titleElem.dataset.klikkikuriConvertedTitle = convertedTitle;
+                                titleElem.dataset.klikkikuriClickbaitLevel = rahtiEntry.clickbaitiness;
+
+                                // Replace the title text.
+                                titleElem.textContent = `${convertedTitle}`;
+                                highlightElemConverted(titleElem);
+
+                                log(`Converted title from '${originalTitle}' to '${convertedTitle}' for link:`, link);
+                            }
+                        } else {
+                            log(`No title element found for rule title selector '${rule.title}' in link:`, link);
+                        }
+                    } else {
+                        log(`No Rahti entry found for hash '${urlHash}' of link:`, link);
+                    }
+
+                });
+            });
+        }
+    };
+
+    const observer = new MutationObserver((mutations) => {
+        // Use original title as the flag, as for example converted title would
+        // be removed when restoring page to show original titles.
+        const isInternalChange = mutations.every(mutation => 
+            mutation.target.dataset.klikkikuriOriginalTitle || 
+            mutation.target.parentElement?.dataset.klikkikuriOriginalTitle
+        );
+        if (isInternalChange) {
+            return;
+        }
+
+        log(`Observed ${mutations.length} DOM mutations, triggering conversion process.`);
+        convertTitles(false).catch((e) => {
+            log("Error during conversion after DOM mutation:", e);
+        });
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: false // Ignore attribute changes to prevent loop from setAttribute
+    });
 
     const convertClickbaits = async (apiUrl, links) => {
         const linkTitleQuerySelectors = await model.read.getLinkTitleQuerySelectors(newsSite);
@@ -231,10 +168,10 @@ const restoreClickbaits = async (links, titleData, linkTitleQuerySelectors) => {
         let convertedTitlesCount;
         if (await model.read.isEnabled(newsSite)) {
             log(`Starting conversion on '${newsSite}'`);
-            convertedTitlesCount = await replaceClickbaits(links, titleData, linkTitleQuerySelectors);
+            convertedTitlesCount = await convertTitles(false);
         } else {
             log(`Restoring original state of ${newsSite} `);
-            await restoreClickbaits(links, titleData, linkTitleQuerySelectors);
+            await convertTitles(true);
             convertedTitlesCount = 0;
         } 
 
@@ -266,92 +203,10 @@ const restoreClickbaits = async (links, titleData, linkTitleQuerySelectors) => {
         }
     });
 
-    const rahti = await rahtiStorage;
-
-    if (!rahti) {
-        log("No Rahti data found, aborting conversion.", rahti);
-        return;
-    }
-
-    const convertTitles = async () => {
-
-        // Run the conversion on reload.
-        const siteRules = await model.read.getSiteRules(newsSite);
-        if (!siteRules) {
-            log(`No site rules found for '${newsSite}', aborting conversion.`);
-        }
-
-        for (const rule of siteRules) {
-            const containers = document.querySelectorAll(rule.container);
-
-            containers.forEach( (container) => {
-                const links = container.querySelectorAll(rule.link);
-                
-                links.forEach(async link => {
-                    // TODO: Might not work on javascript generated links (onclick etc.)
-                    const href = link.getAttribute('href');
-                    if (!href) {
-                        log("Skipping link without href:", link);
-                    }
-                    const urlHash = await hrefSign(href);
-                    // Store for debugging
-                    link.dataset.klikkikuriUrlHash = urlHash;
-
-                    const rahtiEntry = await rahti.get(urlHash)
-                    if (rahtiEntry) {
-                        const titleElem = rule.title ? container.querySelector(rule.title) : link;
-                        if (titleElem) {
-                            const originalTitle = titleElem.textContent;
-                            const convertedTitle = rahtiEntry.title;
-
-                            if (titleElem.dataset.klikkikuriConvertedTitle === convertedTitle) {
-                                // No action needed, already converted before.
-                                return;
-                            }
-
-                            titleElem.dataset.klikkikuriOriginalTitle = originalTitle;
-                            titleElem.dataset.klikkikuriConvertedTitle = convertedTitle;
-                            titleElem.dataset.klikkikuriClickbaitLevel = rahtiEntry.clickbaitiness;
 
 
-                            // Replace the title text.
-                            titleElem.textContent = `${convertedTitle}`;
-                            highlightElemConverted(titleElem);
-
-                            log(`Converted title from '${originalTitle}' to '${convertedTitle}' for link:`, link);
-                        } else {
-                            log(`No title element found for rule title selector '${rule.title}' in link:`, link);
-                        }
-                    } else {
-                        log(`No Rahti entry found for hash '${urlHash}' of link:`, link);
-                    }
-
-                });
-            });
-        }
-    }
-
-    const observer = new MutationObserver((mutations) => {
-        const isInternalChange = mutations.every(mutation => 
-            mutation.target.dataset.klikkikuriConvertedTitle || 
-            mutation.target.parentElement?.dataset.klikkikuriConvertedTitle
-        );
-        if (isInternalChange) {
-            return;
-        }
-
-        log(`Observed ${mutations.length} DOM mutations, triggering conversion process.`);
-        convertTitles().catch((e) => {
-            log("Error during conversion after DOM mutation:", e);
-        });
-    });
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false // Ignore attribute changes to prevent loop from setAttribute
-    });
-
-    await convertTitles();
+    // Run the conversion on reload.
+    await convertTitles(false);
 
     log("Loaded");
 })();
