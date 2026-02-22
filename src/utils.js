487087fc1ccf575a25868d2b7bd85d6abe6375dc
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2026-02-22 14:21:42 +0200

    tweak(logger): Compromise on the logging-variations by having each developer comment out their preferred logger

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,29 @@
 "use strict";
 
+
 const browser = () => (chrome || browser);
 
+const getComplicatedLogger = (name) => {
+    const logInitTime = Date.now();
+    let lastLogTime = logInitTime;
+
+    return (...xs) => {
+        let doTimeDifference;
+        // CONFIG: Comment or uncomment these in order to set different types of
+        // logging TODO: Use some environment flags instead.
+        const thisLogTime = Date.now();
+        doTimeDifference = `⏱️ Δ ${((thisLogTime - lastLogTime) / 1000).toFixed(3)}s ∑ ${((thisLogTime - logInitTime) / 1000).toFixed(3)}s`;
+        lastLogTime = thisLogTime;
+        const message = `[ Loki ⛵ ${name} 🕰️ ${new Date(Date.now()).toISOString()}`;
+        const args = [message, doTimeDifference, "]:"].filter((x) => x !== undefined);
+        console.log.bind(console)(...args, ...xs);
+    };
+};
+
 const getLogger = (name) => {
-    return console.log.bind(console, `[Loki ⛵ ${name} 🕰️ ${new Date(Date.now()).toISOString()}]:`);
+    // CONFIG: Switch the commenting of these different loggings if you like.
+    //return getComplicatedLogger(name);
+    return console.log.bind(console, `[Loki ⛵ ${name}]:`);
 };
 
 const getCurrentTabHostname = async () => {
