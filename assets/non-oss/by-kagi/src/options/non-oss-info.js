a1c51032f6262bc13c548c38887146677d2a498c
Teemu Autto <teemu@rautakuu.org>
2026-08-15 13:00:28 +0300

    feat(non-oss): add Kagi-designed AI badge and permission details

---
+++ assets/non-oss/by-kagi/src/options/non-oss-info.js
@@ -0,0 +1,10 @@
+/**
+ * @file non-oss-info.js
+ * Non-OSS build: Kagi asset credit line.
+ * Overlays src/options/non-oss-info.js in NON_OSS=1 builds.
+ *
+ * @type {string}
+ */
+export const NON_OSS_CREDIT_HTML =
+    'AI smell icon is used with permission, designed by ' +
+    '<a href="https://kagi.com/" target="_blank">Kagi Inc.</a>';
