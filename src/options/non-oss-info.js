9877fcf040770c49b755f1ce9d4e304fc54670ba
Teemu Autto <teemu@rautakuu.org>
2026-08-15 12:59:04 +0300

    feat(about): add About section with runtime info and contributor details

---
+++ src/options/non-oss-info.js
@@ -0,0 +1,11 @@
+/**
+ * @file non-oss-info.js
+ * Non-OSS asset credit line for the About section.
+ *
+ * In OSS builds this is an empty string and nothing is rendered.
+ * In NON_OSS=1 builds, overlaid by
+ * assets/non-oss/by-kagi/src/options/non-oss-info.js.
+ *
+ * @type {string} HTML string, or empty string in OSS builds.
+ */
+export const NON_OSS_CREDIT_HTML = '';
